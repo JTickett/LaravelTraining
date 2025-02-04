171e1c5ba12bfee75df963a2c83dd0bbdf0ef076
James Tickett <jtickett@gmail.com>
2025-02-04 21:04:25 +0000

    update

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,7 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+@props(['active' => false])
+<a 
+class="{{ $active ? 'bg-gray-900 text-white' : 'text-grey-300 hover:bg-grey-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-white"
+aria-current="{{ $active ? 'page' : false }}"
+{{ $attributes }}>
+    {{ $slot }}
+</a>
