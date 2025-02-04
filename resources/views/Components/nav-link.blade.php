d418ba26dfdd31cfc9d264b68294c56ceaeaa525
James Tickett <jtickett@gmail.com>
2025-02-04 21:59:24 +0000

    grey should be gray

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false])
 <a 
-class="{{ $active ? 'bg-gray-900 text-white' : 'text-grey-300 hover:bg-grey-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-white"
+class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-white"
 aria-current="{{ $active ? 'page' : false }}"
 {{ $attributes }}>
     {{ $slot }}
