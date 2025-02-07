c6868a2b43a8268f291f011ece7ed37b830613fb
James Tickett <jtickett@gmail.com>
2025-02-07 12:36:05 +0000

    relationships in Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Employer;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
