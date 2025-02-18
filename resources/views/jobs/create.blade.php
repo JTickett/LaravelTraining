abae901808675aaf1f806ab154847a95504160a9
James Tickett <jtickett@gmail.com>
2025-02-18 14:07:32 +0000

    validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -18,8 +18,13 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="title" id="title"
                                     class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
                             </div>
+
+                            @error('title')
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+
                         </div>
                     </div>
 
@@ -30,8 +35,13 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="salary" id="salary"
                                     class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="£50,000 per year">
+                                    placeholder="£50,000 per year" required>
                             </div>
+
+                            @error('salary')
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+
                         </div>
                     </div>
 
@@ -39,7 +49,7 @@
 
                 </div>
 
-                @if ($errors->any())
+                {{-- @if ($errors->any())
                     <div class="mt-3 text-red-500 italic text-sm">
                         <ul>
                             @foreach ($errors->all() as $error)
@@ -47,7 +57,7 @@
                             @endforeach
                         </ul>
                     </div>
-                @endif
+                @endif --}}
 
             </div>
 
